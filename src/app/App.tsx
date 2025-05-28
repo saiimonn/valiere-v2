e1108683af5e0a58a9d2bcaf6bc7fe35a903782e
Sai <gementizasgg08@gmail.com>
2025-05-28 21:34:37 +0800

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import {SmoothScrollHero} from '../SmoothScrollLenis.tsx'
+import '../styles/index.css'
+
+function App() {
+
+  return (
+    <>
+      <main className = "bg-zinc-950 w-full min-h-screen">
+        <SmoothScrollHero/>        
+      </main>
+    </>
+  )
+}
+
+export default App
